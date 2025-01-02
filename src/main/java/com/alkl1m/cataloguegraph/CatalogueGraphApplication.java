57277bb263686f5ab88a4880364c8a256b905d4f
Александр Климковецкий <al.klimkovetskiy@gmail.com>
2025-01-02 11:12:22 +0200

    chore: Initial commit

---
+++ src/main/java/com/alkl1m/cataloguegraph/CatalogueGraphApplication.java
@@ -0,0 +1,13 @@
+package com.alkl1m.cataloguegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alkl1m/cataloguegraph/CatalogueGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkl1m.cataloguegraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogueGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
